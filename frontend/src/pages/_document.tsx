9af1eafd0ca0960aa6b71f99cf771495937dff70
Júlia D. Craide <juliadcraide@hotmail.com>
2025-06-01 15:27:35 -0300

    Add GoogleAnalytics

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <GoogleAnalytics gaId="G-HK3RR6K8FQ" />
       <body>
         <Main />
         <NextScript />
