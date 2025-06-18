7b2d6ec6f050b2320db059d492544f57d132b548
Júlia D. Craide <juliadcraide@hotmail.com>
2025-06-18 15:10:43 -0300

    Fix header info to add analytics to all pages

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,10 +2,22 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { Head } from "next/document";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Component {...pageProps} />
-    </LocalizationProvider>);
+    <>
+      <Head>
+        <script async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5634805215700413"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <GoogleAnalytics gaId="G-8Z11YZCEQV" />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Component {...pageProps} />
+      </LocalizationProvider>
+    </>
+  );
 }
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -4,13 +4,7 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5634805215700413"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
-      <GoogleAnalytics gaId="G-8Z11YZCEQV" />
+      <Head/>
       <body>
         <Main />
         <NextScript />
