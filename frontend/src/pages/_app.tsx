d6cd16145d0cbb132625e3c879d16190f79ce5bb
Júlia D. Craide <juliadcraide@hotmail.com>
2025-06-18 16:42:21 -0300

    Add google verification

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -13,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5634805215700413"
           crossOrigin="anonymous"
         ></script>
+        <meta name="google-site-verification" content="6kHUAazUt1pl7AUX9JclAoOyeaBG38xERPzOh9EcpNA" />
       </Head>
       <GoogleAnalytics gaId="G-8Z11YZCEQV" />
       <LocalizationProvider dateAdapter={AdapterDayjs}>
